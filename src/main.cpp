51a30c4f88f8619cc6b96aa09d04bddcf3088ddf
Jerry <gao1021229073@163.com>
2023-06-01 23:59:42 +0800

    <feat> 增加GUI、多种光照模式等 * 增加GUI描述渲染设备、OpenGL版本，按下F2显示 * WSAD粘滞 * 鼠标旋转 * 多着色器切换，实现了Phong、Gouraud、Flat三种不同的渲染算法，按下F1切换

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,15 @@
 #include <glm/matrix.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
 #include <iostream>
+extern "C" {
+	_declspec(dllexport) int NvOptimusEnablement = 0x00000001;
+}
+#pragma region 顶点数据
+
 float vertices[] = {
 		-0.93f, 0.00f, -0.36f,  -0.41, -0.25f, -0.00f,
 		-0.58f, -0.58f, -0.58f, -0.41, -0.25f, -0.00f,
@@ -115,10 +123,13 @@ float vertices[] = {
 		0.00f, -0.36f, -0.93f,  -0.25, 0.00f, -0.41f,
 		-0.58f, -0.58f, -0.58f, -0.25, 0.00f, -0.41f,
 };
-unsigned int VAO, VBO;
 
-// 顶点着色器源码
-const char* vertexShaderSource = R"(
+#pragma endregion 顶点数据
+
+unsigned int VAO, VBO;
+#pragma region 着色器源码
+// flat、Phong顶点着色器源码
+const char* vertexShaderSourceFlatPhong = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
     layout (location = 1) in vec3 aNormal;
@@ -137,9 +148,31 @@ const char* vertexShaderSource = R"(
         gl_Position = projection * view * vec4(FragPos, 1.0);
     }
 )";
+// Gouraud顶点着色器源码
+const char* vertexShaderSourceGouraud = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aNormal;
 
-// 片段着色器源码
-const char* fragmentShaderSource = R"(
+    out vec3 FragPos;
+    out vec3 Normal;
+    out vec3 LightDir;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    uniform vec3 lightPos;
+
+    void main()
+    {
+        FragPos = vec3(model * vec4(aPos, 1.0));
+        Normal = mat3(transpose(inverse(model))) * aNormal;
+        LightDir = normalize(lightPos - FragPos);
+        gl_Position = projection * view * vec4(FragPos, 1.0);
+    }
+)";
+// flat 片段着色器源码
+const char* fragmentShaderSourceFlat = R"(
     #version 330 core
     out vec4 FragColor;
 
@@ -159,8 +192,65 @@ const char* fragmentShaderSource = R"(
         FragColor = vec4(diffuse, 1.0);
     }
 )";
+// Phong 片段着色器源码
+const char* fragmentShaderSourcePhong = R"(
+    #version 330 core
+    out vec4 FragColor;
+
+    in vec3 FragPos;
+    in vec3 Normal;
+
+    uniform vec3 lightColor;
+    uniform vec3 lightPos;
+    uniform vec3 objectColor;
+    uniform vec3 viewPos;
+
+    void main()
+    {
+        // Ambient
+        vec3 ambient = lightColor * 0.1;  // 常量环境光照强度
+
+        // Diffuse
+        vec3 norm = normalize(Normal);
+        vec3 lightDir = normalize(lightPos - FragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = diff * lightColor * objectColor;
+
+        // Specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16.0);  // 镜面反射高光强度
+        vec3 specular = spec * lightColor;
+
+        vec3 result = ambient + diffuse + specular;
+        FragColor = vec4(result * objectColor, 1.0);
+    }
+)";
+// Gouraud片段着色器源码
+const char* fragmentShaderSourceGouraud = R"(
+    #version 330 core
+    out vec4 FragColor;
+
+    in vec3 FragPos;
+    in vec3 Normal;
+    in vec3 LightDir;
+
+    uniform vec3 lightColor;
+    uniform vec3 objectColor;
+
+    void main()
+    {
+        // Diffuse
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, LightDir), 0.2);
+        vec3 diffuse = diff * lightColor * objectColor;
+
+        FragColor = vec4(diffuse, 1.0);
+    }
+)";
+#pragma endregion 着色器源码
 // 窗口大小
-const GLuint WIDTH = 1024, HEIGHT = 768;
+const GLuint WIDTH = 1920, HEIGHT = 1080;
 
 // 初始视角
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
@@ -178,6 +268,35 @@ bool firstMouse = true;
 // 键盘相关变量
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
+// 摄像机移动速度
+const float cameraSpeed = 2.5f;
+// 记录上一帧的时间
+float lastFrameTime = 0.0f;
+
+//相机相关变量
+int viewLoc = -255;
+glm::mat4 view;
+glm::vec3 initialCameraPos = glm::vec3(0.0f, 0.0f, 3.0f);   // 相机初始位置
+glm::vec3 initialCameraFront = glm::vec3(0.0f, 0.0f, -1.0f);  // 相机初始朝向
+glm::vec3 initialCameraUp = glm::vec3(0.0f, 1.0f, 0.0f);      // 相机初始上方向
+glm::vec3 targetPos = glm::vec3(0.0f, 0.0f, 0.0f);
+float cameraDistance = 10.0f;
+
+//imgui显示
+bool isShowImGui = false;
+unsigned char* renderer = nullptr;
+unsigned char* version = nullptr;
+
+// 切换光照着色器
+enum SHADER_TYPE
+{
+	Flat = 0x00,
+	Phong = 0x01,
+	Gouraud = 0x02
+};
+//光照着色器选择
+int shaderType = SHADER_TYPE::Flat;
+
 
 // 鼠标移动回调函数
 void mouseCallback(GLFWwindow* window, double xPos, double yPos)
@@ -188,48 +307,154 @@ void mouseCallback(GLFWwindow* window, double xPos, double yPos)
 		lastY = yPos;
 		firstMouse = false;
 	}
+	// 检查鼠标左键是否按下
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && viewLoc != -255)
+	{
+		// 计算鼠标位置的偏移量
+		double xOffset = xPos - lastX;
+		double yOffset = lastY - yPos;
+		lastX = xPos;
+		lastY = yPos;
 
-	GLfloat xOffset = xPos - lastX;
-	GLfloat yOffset = lastY - yPos;
-	lastX = xPos;
-	lastY = yPos;
+		// 设置旋转速度
+		float rotationSpeed = 0.1f;
 
-	GLfloat sensitivity = 0.05f;
-	xOffset *= sensitivity;
-	yOffset *= sensitivity;
+		// 计算旋转角度
+		float angleX = xOffset * rotationSpeed;
+		float angleY = yOffset * rotationSpeed;
 
-	yaw += xOffset;
-	pitch += yOffset;
+		// 构建旋转矩阵
+		glm::mat4 rotationMatrixX = glm::rotate(glm::mat4(1.0f), glm::radians(angleX), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 rotationMatrixY = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(1.0f, 0.0f, 0.0f));
+		glm::mat4 rotationMatrix = rotationMatrixX * rotationMatrixY;
 
-	if (pitch > 89.0f)
-		pitch = 89.0f;
-	if (pitch < -89.0f)
-		pitch = -89.0f;
+		// 将摄像机位置绕原点旋转
+		glm::vec4 cameraPos4 = glm::vec4(cameraPos, 1.0f);
+		glm::vec4 rotatedCameraPos4 = rotationMatrix * cameraPos4;
+		cameraPos = glm::vec3(rotatedCameraPos4);
 
-	glm::vec3 front;
-	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	front.y = sin(glm::radians(pitch));
-	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-	cameraFront = glm::normalize(front);
+		// 更新相机矩阵
+		view = glm::lookAt(cameraPos, glm::vec3(0.0f), cameraUp);
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	}
+	else
+	{
+
+		GLfloat xOffset = xPos - lastX;
+		GLfloat yOffset = lastY - yPos;
+		lastX = xPos;
+		lastY = yPos;
+
+		GLfloat sensitivity = 0.05f;
+		xOffset *= sensitivity;
+		yOffset *= sensitivity;
+
+		yaw += xOffset;
+		pitch += yOffset;
+
+		if (pitch > 89.0f)
+			pitch = 89.0f;
+		if (pitch < -89.0f)
+			pitch = -89.0f;
+
+		glm::vec3 front;
+		front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		front.y = sin(glm::radians(pitch));
+		front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cameraFront = glm::normalize(front);
+	}
+
 }
 
+// 滚轮回调函数
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	// 根据滚轮的偏移量计算缩放因子
+	float scaleFactor = 1.0f + yOffset * 0.1f;
+
+	// 调整摄像机位置以实现缩放效果
+	cameraPos *= scaleFactor;
+
+}
 // 键盘输入回调函数
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	// 获取当前帧的时间
+	float currentFrameTime = glfwGetTime();
+	// 计算时间间隔
+	float deltaTime = currentFrameTime - lastFrameTime;
+	lastFrameTime = currentFrameTime;
+	//printf("delta time:%lf\n", deltaTime);
+	// 速度乘以时间间隔，使移动速度平滑
+	float velocity = cameraSpeed * deltaTime;
 
-	GLfloat cameraSpeed = 125.0f * deltaTime;
-	if (key == GLFW_KEY_W)
-		cameraPos += cameraSpeed * cameraFront;
-	if (key == GLFW_KEY_S)
-		cameraPos -= cameraSpeed * cameraFront;
-	if (key == GLFW_KEY_A)
-		cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
-	if (key == GLFW_KEY_D)
-		cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+	//0.4f 为持久按下键的事件时间，可以防止按下一个键之后的误触。
+	//此时间可能需要调整，根据不同的渲染环境。
+	if (deltaTime <= 0.4f)
+	{
+		// 按下WSAD键时改变摄像机位置
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			cameraPos += velocity * cameraFront;
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			cameraPos -= velocity * cameraFront;
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * velocity;
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * velocity;
+	}
+	if (key == GLFW_KEY_F2 && action == GLFW_PRESS)
+	{
+		isShowImGui = !isShowImGui;  // 切换ImGui的可见性状态
+	}
+	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+	{
+		shaderType++;
+		if (shaderType == 0x03)
+		{
+			shaderType = 0x00;
+		}
+	}
 }
+// 回调函数：渲染ImGui的内容
+void ImGuiRender()
+{
+	// 渲染ImGui
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
 
+	// 在ImGui中渲染内容
+	ImGui::SetWindowFontScale(2.0); // 设置字体缩放比例
+	ImGui::SetNextWindowPos(ImVec2(10, 10));
+	switch (shaderType)
+	{
+	case Phong:
+		ImGui::Text("Phong shader");
+		break;
+	case Gouraud:
+		ImGui::Text("Gouraud shader");
+		break;
+	case Flat:
+	default:
+		ImGui::Text("Flat/Default shader");
+		break;
+	}
+	if (renderer != nullptr)
+	{
+		ImGui::Text("%s", renderer);
+	}
+	if (version != nullptr)
+	{
+		ImGui::Text("%s", version);
+	}
+	ImGui::Text("Frame Rate: %.1f FPS", ImGui::GetIO().Framerate);
+	ImGui::Text("X:%.1f,Y:%.1f,Z:%.1f", cameraPos[0], cameraPos[1], cameraPos[2]);
+	// 完成ImGui的渲染
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
 
 // 编译着色器
 unsigned int compileShader(unsigned int type, const char* source)
@@ -252,7 +477,7 @@ unsigned int compileShader(unsigned int type, const char* source)
 }
 
 // 创建着色器程序
-unsigned int createShaderProgram()
+unsigned int createShaderProgram(const char* vertexShaderSource , const char* fragmentShaderSource)
 {
 	unsigned int vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
 	unsigned int fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -284,7 +509,7 @@ int main()
 	glfwInit();
 
 	// 创建窗口
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Flat Shading Cube", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Real Shading Dodecahedron", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		glfwTerminate();
@@ -300,6 +525,17 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	// 获取渲染设备信息
+
+	renderer = (unsigned char*)glGetString(GL_RENDERER);
+	version = (unsigned char*)glGetString(GL_VERSION);
+
+	// 初始化ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330 core");
 
 	// 创建 VAO 和 VBO
 	glGenVertexArrays(1, &VAO);
@@ -323,34 +559,61 @@ int main()
 	glBindVertexArray(0);
 
 	// 创建着色器程序
-	unsigned int shaderProgram = createShaderProgram();
+	unsigned shaderProgram = 0;
+	unsigned int FlatShaderProgram = createShaderProgram(vertexShaderSourceFlatPhong, fragmentShaderSourceFlat);
+	unsigned int PhongShaderProgram = createShaderProgram(vertexShaderSourceFlatPhong, fragmentShaderSourcePhong);
+	unsigned int GouraudShaderProgram = createShaderProgram(vertexShaderSourceGouraud, fragmentShaderSourceGouraud);
 	// 注册回调函数
 	glfwSetCursorPosCallback(window, mouseCallback);
 	glfwSetKeyCallback(window, keyCallback);
-
+	glfwSetScrollCallback(window, scrollCallback);
 	// 隐藏并捕捉鼠标
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	// 主循环
+
+	//启用深度测试
+	glEnable(GL_DEPTH_TEST);
+	//启用面剔除
+	glEnable(GL_CULL_FACE);
+	//启用多重采样抗锯齿（可选）
+	glfwWindowHint(GLFW_SAMPLES, 16);
+	glEnable(GL_MULTISAMPLE);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		GLfloat currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
-		glEnable(GL_DEPTH_TEST); 
+
 		// 清空颜色缓冲和深度缓冲
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// 使用着色器程序
-		glUseProgram(shaderProgram);
+		switch (shaderType)
+		{
+		case Phong:
+			glUseProgram(PhongShaderProgram);
+			shaderProgram = PhongShaderProgram;
+			break;
+		case Gouraud:
+			glUseProgram(GouraudShaderProgram);
+			shaderProgram = GouraudShaderProgram;
+			break;
+		case Flat:
+		default:
+			glUseProgram(FlatShaderProgram);
+			shaderProgram = FlatShaderProgram;
+			break;
+		}
 
 		// 设置着色器中的 uniform 变量
 		int objectColorLoc = glGetUniformLocation(shaderProgram, "objectColor");
 		int lightColorLoc = glGetUniformLocation(shaderProgram, "lightColor");
 		int lightPosLoc = glGetUniformLocation(shaderProgram, "lightPos");
 		int modelLoc = glGetUniformLocation(shaderProgram, "model");
-		int viewLoc = glGetUniformLocation(shaderProgram, "view");
+		viewLoc = glGetUniformLocation(shaderProgram, "view");
 		int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
-
+		int viewPos = glGetUniformLocation(shaderProgram, "viewPos");
 		glUniform3f(objectColorLoc, 0.2f, 0.4f,0.2f);
 		glUniform3f(lightColorLoc, 1.0f, 1.0f, 1.0f);
 		glUniform3f(lightPosLoc, 16.0f, 16.0f, 16.0f);
@@ -362,13 +625,11 @@ int main()
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		glfwPollEvents();
-		glm::mat4 view;
 		// 更新相机矩阵
-		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		// 计算视图矩阵
-		//view = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniform3f(viewPos, cameraPos.x, cameraPos.y, cameraPos.z);
 
 		// 计算模型矩阵
 		glm::mat4 model = glm::mat4(1.0f);
@@ -380,20 +641,19 @@ int main()
 		// 绘制正方体
 		glDrawArrays(GL_TRIANGLES, 0, 108);
 
-		 
-		//glBindVertexArray(VAO);
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		//glColor3f(1.0, 1.0, 1.0);  // 设置线框的颜色为白色
-		//glLineWidth(2.0);  // 设置线宽
-		//// 绘制正方体
-		//glDrawArrays(GL_TRIANGLES, 0, 108);
-		//glEnd();
 
 		// 解绑 VAO
 		glBindVertexArray(0);
 
+		if (isShowImGui)
+		{
+			ImGuiRender();
+		}
+
 		// 交换缓冲并检查事件
 		glfwSwapBuffers(window);
+		glfwPollEvents();
+
 
 	}
 
@@ -401,6 +661,11 @@ int main()
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteProgram(shaderProgram);
+
+	// 清理ImGui
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	// 终止 GLFW
 	glfwTerminate();
